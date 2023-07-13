bceddeab2e51844281de64f3be18358616aa281d
Shalini Sihag <shalini.sihag.eee20@itbhu.ac.in>
2023-07-13 23:24:56 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import '@styles/globals.css';
+import { Children } from 'react';
+
+import Nav from '@components/Nav';
+import Provider from '@components/Provider'
+export const metadata= {
+    title: "Promptverse",
+    description: ' Where AI Meets Creativity'
+}
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+        <body>
+          <Provider>
+            <div className ="main">
+                <div className="gradient" />
+            </div>
+            <main className="app">
+              <Nav/>
+               {children} 
+            </main>
+          </Provider>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
